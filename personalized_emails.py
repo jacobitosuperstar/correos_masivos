7787c848657edec06a690dc971e958e6626ee6b8
Jacobitosuperstar <jacobomateobedoya@icloud.com>
2020-07-26 18:44:57 -0500

    finalizado el programa que envía correos con archivos adjuntos propios por usuario

--- personalized_emails.py
+++ personalized_emails.py
@@ -3,7 +3,21 @@ import imghdr
 from email.message import EmailMessage
 import csv
 
-def send_email(send_to):
+with open("prueba.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    next(csv_reader)
+    datos = list(csv_reader)
+
+#with open("correos.csv") as csv_file:
+#    csv_reader = csv.reader(csv_file, delimiter=",")
+#    correos_list = list(csv_reader)
+
+#correos = "".join(str(correo) for correo in correos_list)
+
+#print(correos_list[5])
+
+for documento,correo,nombres,apellidos in datos:
+
     # exchange Sign In
     exchange_sender = "becassapiencia@itm.edu.co"
     exchange_passwd = "Seguimiento2018"
@@ -11,14 +25,15 @@ def send_email(send_to):
     msg = EmailMessage()
     msg["from"] = exchange_sender
     msg["subject"] = 'comunicado Becas Tecnologías Alcaldía de Medellín'
-    msg["Bcc"] = send_to
+    msg["Bcc"] = correo
 
     ### ESTE ES EL CUERPO DEL COMUNICADO QUE VAMOS A ENVIAR ###
-    msg.set_content("Hola,Hemos ampliado las fechas de inscripción hasta el 4 de junio. No pierdas esta oportunidad.")
+    msg.set_content(f"Hola, {nombres} {apellidos} Este es el correo de prueba para probar el envío de contenido dinámico.")
 
     ### ESTE ES LO QUE SE VA A HACER PARA AGREGAR PDFs A LOS CORREOS ###
-
-    files = ["guía_habilitados_becas_tecnologías.pdf"]
+    file_acta=f"{documento}_acta.pdf"
+    file_diploma=f"{documento}_diploma.pdf"
+    files = [file_acta,file_diploma]
 
     for file in files:
         with open(file,"rb") as f:
@@ -29,16 +44,15 @@ def send_email(send_to):
 
     ### ESTO ES LO QUE SE VA HACER PARA AGREGAR IMÁGENES A LOS CORREOS ###
 
-    files = ["oferta_académica.jpeg"]
-
-    for file in files:
-        with open(file,"rb") as f:
-            file_data = f.read()
-            file_type = imghdr.what(f.name)
-            file_name = f.name
-
-        msg.add_attachment(file_data, maintype="image", subtype=file_type, filename=file_name)
-
+#    files = ["oferta_académica.jpeg"]
+#
+#    for file in files:
+#        with open(file,"rb") as f:
+#            file_data = f.read()
+#            file_type = imghdr.what(f.name)
+#            file_name = f.name
+#
+#        msg.add_attachment(file_data, maintype="image", subtype=file_type, filename=file_name)
 
     server = smtplib.SMTP("smtp.office365.com",587)
     server.ehlo()
@@ -47,22 +61,7 @@ def send_email(send_to):
 
     try:
         server.send_message(msg)
-        print ('email sent')
+        print (f"email sent to {nombres}")
     except:
-        print ('error sending mail')
+        print (f"error sending mail {nombres}")
     server.quit()
-
-with open("prueba.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    correos_list = list(csv_reader)
-
-#with open("correos.csv") as csv_file:
-#    csv_reader = csv.reader(csv_file, delimiter=",")
-#    correos_list = list(csv_reader)
-
-#correos = "".join(str(correo) for correo in correos_list)
-
-#print(correos_list[5])
-
-for correo in correos_list:
-    send_email(correo)
