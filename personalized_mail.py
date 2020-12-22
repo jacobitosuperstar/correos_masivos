8601f675a4b22b59ce96bcac339a068f94e9273e
Jacobo <jacobomateobedoya@icloud.com>
2020-12-22 14:27:32 -0500

    correos masivos ITM 22 de diciembre

--- personalized_mail.py
+++ personalized_mail.py
@@ -4,7 +4,7 @@ from email.message import EmailMessage
 import csv
 import os
 
-with open("pruebas_itm/CSV_de_prueba.csv") as csv_file:
+with open("once_am.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=";")
     next(csv_reader)
     datos = list(csv_reader)
@@ -17,7 +17,7 @@ with open("pruebas_itm/CSV_de_prueba.csv") as csv_file:
 
 #print(correos_list[5])
 
-for Nombre,Correo,Documento in datos:
+for Programa,Nombre,Correo,Documento in datos:
 
     ## SIGN IN ##
     EMAIL_SENDER = os.environ["EMAIL_SENDER"]
@@ -26,15 +26,15 @@ for Nombre,Correo,Documento in datos:
 
     msg = EmailMessage()
     msg["from"] = EMAIL_SENDER
-    msg["subject"] = 'Correo de prueba para el envio de información dinámico'
+    msg["subject"] = f"Grados {Nombre}"
     msg["Bcc"] = Correo
 
     ### ESTE ES EL CUERPO DEL COMUNICADO QUE VAMOS A ENVIAR ###
     msg.set_content(f"Hola, {Nombre} Este es el correo de prueba para probar el envío de contenido dinámico.")
 
     ### ESTE ES LO QUE SE VA A HACER PARA AGREGAR PDFs A LOS CORREOS ###
-    file_acta=f"pruebas_itm/Arte_Actas/{Documento}.pdf"
-    file_diploma=f"pruebas_itm/Arte_Diplomas/{Documento}.pdf"
+    file_acta=f"../Acta{Documento}.pdf"
+    file_diploma=f"../Diploma{Documento}.pdf"
     files = [file_acta,file_diploma]
 
     for file in files:
@@ -46,15 +46,15 @@ for Nombre,Correo,Documento in datos:
 
     ### ESTO ES LO QUE SE VA HACER PARA AGREGAR IMÁGENES A LOS CORREOS ###
 
-#    files = ["oferta_académica.jpeg"]
-#
-#    for file in files:
-#        with open(file,"rb") as f:
-#            file_data = f.read()
-#            file_type = imghdr.what(f.name)
-#            file_name = f.name
-#
-#        msg.add_attachment(file_data, maintype="image", subtype=file_type, filename=file_name)
+    files = ["felicidades_egresados.jpg"]
+
+    for file in files:
+        with open(file,"rb") as f:
+            file_data = f.read()
+            file_type = imghdr.what(f.name)
+            file_name = f.name
+
+        msg.add_attachment(file_data, maintype="image", subtype=file_type, filename=file_name)
 
     server.ehlo()
     server.starttls()
