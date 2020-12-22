81529d2dfbffe29bee9cd3039c9bd13e60eb0a8d
Jacobo <jacobomateobedoya@icloud.com>
2020-12-22 16:20:53 -0500

    finalizando la muestra de errores de los archivos

--- personalized_mail.py
+++ personalized_mail.py
@@ -4,7 +4,7 @@ from email.message import EmailMessage
 import csv
 import os
 
-with open("once_am.csv") as csv_file:
+with open("dos_pm.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=";")
     next(csv_reader)
     datos = list(csv_reader)
@@ -33,28 +33,33 @@ for Programa,Nombre,Correo,Documento in datos:
     msg.set_content(f"")
 
     ### ESTE ES LO QUE SE VA A HACER PARA AGREGAR PDFs A LOS CORREOS ###
-    file_acta=f"../Acta{Documento}.pdf"
-    file_diploma=f"../Diploma{Documento}.pdf"
-    files = [file_acta,file_diploma]
+    try:
+        file_acta=f"../Acta{Documento}.pdf"
+        file_diploma=f"../Diploma{Documento}.pdf"
+        files = [file_acta,file_diploma]
 
-    for file in files:
-        with open(file,"rb") as f:
-            file_data = f.read()
-            file_name = f.name
+        for file in files:
+            with open(file,"rb") as f:
+                file_data = f.read()
+                file_name = f.name
 
-        msg.add_attachment(file_data, maintype="application", subtype="octet-stream", filename=file_name)
+            msg.add_attachment(file_data, maintype="application", subtype="octet-stream", filename=file_name)
+    except:
+        print(f"error con los adjuntos del acta o diploma de {Nombre}, con el docmento {Documento}")
 
     ### ESTO ES LO QUE SE VA HACER PARA AGREGAR IMÁGENES A LOS CORREOS ###
+    try:
+        files = ["felicidades_egresados.jpg"]
 
-    files = ["felicidades_egresados.jpg"]
+        for file in files:
+            with open(file,"rb") as f:
+                file_data = f.read()
+                file_type = imghdr.what(f.name)
+                file_name = f.name
 
-    for file in files:
-        with open(file,"rb") as f:
-            file_data = f.read()
-            file_type = imghdr.what(f.name)
-            file_name = f.name
-
-        msg.add_attachment(file_data, maintype="image", subtype=file_type, filename=file_name)
+            msg.add_attachment(file_data, maintype="image", subtype=file_type, filename=file_name)
+    except:
+        print(f"error adjuntando la imagen de {Nombre}, con el documento {Documento}")
 
     server.ehlo()
     server.starttls()
@@ -62,9 +67,9 @@ for Programa,Nombre,Correo,Documento in datos:
 
     try:
         server.send_message(msg)
-        print (f"email sent to {Nombre}")
+        print (f"email sent to {Nombre}, with CC {Documento}")
     except:
-        print (f"error sending mail {Nombre}")
+        print (f"error sending mail {Nombre}, with CC {Documento}")
     server.quit()
 
-print("Finalizado el envío de información de prueba")
+print("Finalizado el envío de información")
